64f84005f183e56a258b51cf295d472541320271
Alan Houston <alanhouston5000@gmail.com>
2019-05-04 21:09:25 -0500

    button styling

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import './landingPage.css';
 
 class landingPage extends React.Component {
@@ -6,28 +8,29 @@ class landingPage extends React.Component {
         return (
             <div>
                 <header>
+                {/* THis goddamn image doesnt load */}
                 <img src="logoGP.png" alt="Grace Place logo"/>
                     <div className="logo">
                         <img src="https://careportal.org/wp-content/uploads/2019/01/careportal-logo.png" alt="Care Portal Logo"/>
                     </div>
                 </header>
                 <main>
-                    <h1>Search For Your Saving Grace</h1>
+                    <h1>Search For Your Saving Grace!</h1>
                     <div className="time">
                         <h3>Donate Your Time</h3>
-                        <button className="b0"> Donate Time </button>
+                        <Link className="center" to= "/form"><button className="b0">Donate Time </button></Link>
                     </div>
                     <div className="thing">
-                        <h3>I Have an Item(s) to Donate</h3>
-                        <button className="b1">Donate Item</button>
+                        <h3>I Have an Item(s) to Donate!</h3>
+                        <Link className="center" to="/form"><button className="b1">Donate Item</button></Link>
                     </div>
                     <div className="trade">
-                        <h3>Im Skilled!</h3>
-                        <button className="b0">Donate Your Skills</button>
+                        <h3>I'm Skilled!</h3>
+                        <Link className="center" to="/form"><button className="b0">Donate Your Skills</button></Link>
                     </div>
                     <div className="help">
-                        <h3>I just want to help</h3>
-                        <button className="b1">Get Started</button>
+                        <h3>I just want to help!</h3>
+                        <Link className="center" to="/form" ><button className="b1">Get Started</button></Link>
                     </div>
                 </main>
             </div>
